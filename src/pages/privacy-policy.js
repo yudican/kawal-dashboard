969c96a7207e6c1c9f296d8a1f04e8bd7f7d4160
Yudi Candra <yudhichandra02@gmail.com>
2023-11-14 20:00:41 +0700

    add -privacy policy

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,68 @@
+const PrivacyPolicy = () => {
+  return (
+    <p>
+      <h1>1. Introduction</h1>
+
+      <p>
+        This Privacy Policy ("Policy") describes how Markdata ("we," "us," or "our") collects, uses, and discloses your
+        personal information when you use our political canvassing app, Ir-1,the ("App"). By using the App, you agree to
+        the terms of this Policy. If you do not agree to this Policy, you should not use the App.
+      </p>
+
+      <h1>2. Information We Collect</h1>
+
+      <p>
+        We collect the following information from your use of the App: Personal Information: We collect personal
+        information from you when you voluntarily provide it to us, such as your name, address, email address, and phone
+        number. Device Information: We collect information about the device you use to access the App, such as the
+        device type, operating system, and IP address. Usage Data: We collect usage data about your use of the App, such
+        as the pages you visit, the features you use, and the time you spend using the App.
+      </p>
+
+      <h1>3. How We Use Your Information</h1>
+
+      <p>
+        We use your information for the following purposes: To provide you with the App: We use your information to
+        provide you with the App and its features. To improve the App: We use your information to improve the App and
+        its features. To communicate with you: We use your information to communicate with you about the App, such as to
+        send you updates or notifications. To comply with law: We use your information to comply with applicable laws
+        and regulations.
+      </p>
+
+      <h1>4. Sharing Your Information</h1>
+
+      <p>
+        We share your information with the following third parties: Service providers: We share your information with
+        service providers who help us operate the App, such as hosting providers and analytics providers. Law
+        enforcement: We share your information with law enforcement agencies if we are required to do so by law.
+      </p>
+
+      <h1>5. Your Choices</h1>
+
+      <p>
+        You have the following choices regarding your information: You can access and update your personal information:
+        You can access and update your personal information by logging into your account and going to the "Settings"
+        page. You can opt out of receiving marketing communications: You can opt out of receiving marketing
+        communications from us by clicking on the "unsubscribe" link in any email marketing communication we send you.
+      </p>
+
+      <h1>6. Security</h1>
+
+      <p>
+        We take the security of your information seriously and have implemented appropriate safeguards to protect it
+        from unauthorized access, use, disclosure, alteration, or destruction.
+      </p>
+
+      <h1>7. Changes to This Policy</h1>
+
+      <p>
+        We may update this Policy from time to time. We will notify you of any material changes by posting the updated
+        Policy on our website or in the App.
+      </p>
+
+      <h1>8. Contact Us</h1>
+
+      <p>If you have any questions about this Policy, please contact us at web@markdata.id</p>
+    </p>
+  )
+}
